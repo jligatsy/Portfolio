6d3947e6a407742b540f602cb8f7f0513b4b80b1
jligatsy <jligatsy@uci.edu>
2025-03-30 00:53:34 -0700

    force scroll to home?

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import Project from "./components/Project";
 import Experiences from "./components/Experiences"
 import Footer from "./components/Footer"
 import "./styles/global.css"; 
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <Navbar />
